bbc4a7d7b4ede8785dc591a2855bffcc700a15b2
khushboo <ksingh200601@gmail.com>
2024-10-04 19:29:29 +0530

    implemented pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
+import LandingPage from './Pages/LandingPage/LandingPage'
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
+import Courses from './Pages/Courses/Courses'
+import Contact from './Pages/Contact/Contact'
+import Login from './Pages/Login/Login'
+import Signup from './Pages/SignUp/Signup'
 const App = () => {
   return (
-    <div>App</div>
+ 
+   <Routes>
+    <Route index elememt={<LandingPage/>}/>
+    <Route path='/landing' element={<LandingPage/>}/>
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/signUp' element={<Signup/>}/>
+    <Route path='/home' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/courses' element={<Courses/>}/>
+    <Route path='/contact' element={<Contact/>}/>
+
+   </Routes>
+
   )
 }
 
