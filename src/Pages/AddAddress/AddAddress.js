f0429f13afd52098bda7bd6f32408b250772cc71
khushboo kumari <ksingh200601@gmail.com>
2024-11-22 00:49:02 +0530

    userProfile and add address page layout implemented

--- src/Pages/AddAddress/AddAddress.js
+++ src/Pages/AddAddress/AddAddress.js
@@ -1,8 +1,51 @@
 import React from 'react'
 import './AddAddress.css'
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import { Button, Divider, TextField } from '@mui/material';
 const AddAddress = () => {
   return (
-    <div className='add-address-container'>AddAddress</div>
+    <div className='add-address-container'>
+      <div className='add-address-box'>
+        <div className='add-address-title'>
+          <div><KeyboardBackspaceIcon/></div>
+          <h4>ADDRESS</h4>
+          </div> 
+          <div className='add-new-address-title'>
+            <h5>ADD NEW ADDRESS</h5>
+            <Divider />
+          </div>
+          <div className='delivering-name-mobile'>
+          <TextField id="Name" label="Name" variant="standard" fullWidth required />
+          <TextField id="Mobile" label="Mobile" variant="standard" fullWidth required sx={{marginTop:2}} />
+          </div>
+          <div className='delivering-rest-Details'>
+           <div className='delivering-pincode-state'>
+           <TextField id="pincode" label="pincode" variant="standard"  required  />
+           <TextField id="state" label="state" variant="standard" required  />
+           </div>
+           <TextField id="street" label="Address(street,near By Area)" variant="standard" fullWidth required sx={{marginTop:2}} />
+           <TextField id="city" label="city/Town" variant="standard" fullWidth required sx={{marginTop:2}} />
+          </div>
+          <div className='type-of-address'>
+            <h4>Type of Address</h4>
+            <form>
+              <label>
+                <input type='radio' value='home' name="home"/>
+                Home
+              </label>
+              <label>
+                <input type='radio' value="work"  name="work"/>
+                Work
+              </label>
+             </form>
+            
+          </div>
+          <div className='buttons'>
+             <Button variant='contained'>Submit</Button>
+          </div>
+         
+      </div>
+    </div>
   )
 }
 
--- src/Pages/UserProfile/UserProfile.js
+++ src/Pages/UserProfile/UserProfile.js
@@ -1,13 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import './UserProfile.css'
 import { Usercontext } from '../../App'
 import { deepOrange } from '@mui/material/colors';
 import { Avatar, Button, Divider} from '@mui/material';
 import { useNavigate } from 'react-router';
+import axios from 'axios';
 const UserProfile = () => {
     const user=useContext(Usercontext)
     console.log(user)
+    const [addressList,setAddressList]=useState([])
     const navigate =useNavigate()
+    React.useEffect(()=>{
+    const getAddress=async()=>{
+     try{
+     const address=await axios.get(`http://localhost:8888/thirdProject/api/v1/user/${user._id}/getAllAddresses`)
+     const addressResponse=address?.data;
+     setAddressList(addressResponse)
+     }
+     catch(e){
+       console.log(e)
+     }
+    }
+    getAddress()
+    },[user?._id])
+
+    console.log(addressList)
   return (
     <div className='user-profile-container'>
       <div className='user-profile-box'>
@@ -16,8 +33,8 @@ const UserProfile = () => {
            <Avatar sx={{ bgcolor: deepOrange[500], width: 50,height:50, }}>N</Avatar>
            </div>
            <div className='user-details'>
-             <div>khushboo singh</div>
-             <div>ksunghjnbh</div>
+             <div>{user?.name.toUpperCase()}</div>
+             <div>{user?.email}</div>
            </div>
           
          </div>
@@ -29,6 +46,28 @@ const UserProfile = () => {
           <Button variant="contained" sx={{marginRight:2}} onClick={()=>navigate('/userProfile/addAddress')}>Add </Button>
          <Button variant="contained">Edit</Button>
          </div>
+         <div className='address-part'>
+          {
+            addressList.length===0?'No Address added':
+            addressList.map((addressDetails,index)=>{
+              return  <div className='address-box' key={addressDetails?._id}>
+                <div className='delivering-details'>
+              <h5>{addressDetails?.name}</h5>
+              <p>{addressDetails?.street}</p>
+              <p>{addressDetails?.city}</p>
+              <p>{addressDetails?.pincode}</p>
+              <p>{addressDetails?.state}</p>
+             <div className='mobile'>
+             <span>Mobile No.-</span>
+             <span>{addressDetails?.mobileNo}</span>
+             </div>
+
+           </div>
+            <div className='address-type'>{addressDetails?.typeOfAddress}</div>
+            </div>
+            })
+          }
+          </div>
         </div>
       </div>
        
