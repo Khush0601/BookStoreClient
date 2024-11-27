73e79cfb208090a903babdbfbe87e99b0cc39104
khushboo kumari <ksingh200601@gmail.com>
2024-11-27 21:34:56 +0530

    implemented default address in reviewOrder page

--- src/Pages/ReviewYourOrder/ReviewYourOrder.js
+++ src/Pages/ReviewYourOrder/ReviewYourOrder.js
@@ -1,19 +1,38 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './ReviewYourOrder.css'
 import { useLocation } from 'react-router'
 import { useNavigate } from 'react-router'
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
-import { Divider } from '@mui/material';
+import { Button, Divider } from '@mui/material';
+import { Usercontext } from '../../App';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
  const ReviewYourOrder = () => {
+  const user=useContext(Usercontext)
+  console.log('user',user)
   const navigate=useNavigate()
   const data=useLocation()
   let productData=data.state
   console.log(productData)
   
   const [orderFullDetails,setOrderFullDetails]=useState(()=>productData)
-  
+  const [defaultAddress,setDefaultAddress]=useState({})
+  React.useEffect(()=>{
+    const getDefaultAddress=async()=>{
+     try{
+    const address=await axios.get(`http://localhost:8888/thirdProject/api/v1/user/${user?._id}/getDefaultAddresses`)
+    const addressResponse=address?.data
+    setDefaultAddress(addressResponse)
+     }
+     catch(e){
+      console.log(e?.response?.statusText)
+     }
+    }
+    getDefaultAddress()
+  },[user?._id])
   console.log('orderDetails',orderFullDetails)
+  console.log('defaultAddress',defaultAddress)
   return (
     <div className='review-order-container'>
       <div className='review-order-box'>
@@ -33,8 +52,24 @@ import { Divider } from '@mui/material';
             <p>Qty:1</p>
           </div>
         </div>
+        <div className='deliver-address'><h4>Delivery Address</h4></div>
+        <Divider/>
         <div className='select-address'>
-         default address
+        <div>
+        <h4>{defaultAddress?.name}</h4>
+        <p>{defaultAddress?.mobileNo}</p>
+        <p>{defaultAddress?.street}</p>
+        <p>{defaultAddress?.city}</p>
+        <p>{defaultAddress?.state}</p>
+        <p>{defaultAddress?.pincode}</p>
+        <Link to='/userProfile'>Change Address</Link>
+        </div>
+        <div>
+          {defaultAddress?.typeOfAddress}
+        </div>
+        </div>
+        <div className='proceed-button'>
+          <Button variant='contained'>Proceed For Payment</Button>
         </div>
       </div>
     </div>
